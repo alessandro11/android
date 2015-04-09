aa4c8671efd5527e290670297a5d2ee007897864
Alessandro Elias <ae11@c3sl.ufpr.br>
2015-04-09 00:11:27 -0300

    Customizing To-Do List Pg 135.
    Signed-off-by: Alessandro Elias <ae11@c3sl.ufpr.br>

---
+++ ToDoListCustom/app/src/main/java/com/pg136/ae11/todolistcustom/ToDoListItemView.java
@@ -0,0 +1,59 @@
+package com.pg136.ae11.todolistcustom;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+
+/**
+ * Created by m3cool on 07/04/15.
+ */
+public class ToDoListItemView extends TextView {
+    private Paint marginPaint;
+    private Paint linePaint;
+    private int paperColor;
+    private float margin;
+
+    public ToDoListItemView(Context context, AttributeSet ats, int ds) {
+        super(context, ats, ds);
+        init();
+    }
+
+    public ToDoListItemView (Context context) {
+        super(context);
+        init();
+    }
+
+    public ToDoListItemView (Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        // Get reference to our resource table.
+        Resources myResources = getResources();
+
+        // Create the paint brushes we will use in the onDraw method.
+        marginPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        marginPaint.setColor(myResources.getColor(R.color.notepad_margin));
+        linePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        linePaint.setColor(myResources.getColor(R.color.notepad_lines));
+
+        // Get the paper background color and the margin width.
+        paperColor = myResources.getColor(R.color.notepad_paper);
+        margin = myResources.getDimension(R.dimen.notepad_margin);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        // Color as paper
+        canvas.drawColor(paperColor);
+
+        // Draw ruled 
+        // Use the base TextView to render the text.
+        super.onDraw(canvas);
+    }
+}
